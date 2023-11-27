c3b92fb1683ac5e22db278aa0f4a5bcd920d65e8
arthur levesque <arthurlevesque35@gmail.com>
2023-11-27 20:35:28 +0100

    Initialisation de l'application

---
+++ src/test/java/com/openclassrooms/apichatop/ApiChatopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.apichatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChatopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
