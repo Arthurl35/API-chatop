33e505a14c5d7f0a1858c9bcc4bc0805bd54d33d
arthur levesque <arthurlevesque35@gmail.com>
2023-12-02 19:44:11 +0100

    Création du repository User pour récupérer l'utilisateur via son email qui est propre à lui même.

--- src/main/java/com/openclassrooms/apichatop/model/User.java
+++ src/main/java/com/openclassrooms/apichatop/model/User.java
@@ -7,12 +7,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Data; 
 
 @Data
 @Entity
 @Table(name="USERS")
-public class DBUser {
+public class User {
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
--- src/main/java/com/openclassrooms/apichatop/services/JWTService.java
+++ src/main/java/com/openclassrooms/apichatop/services/JWTService.java
@@ -10,27 +10,31 @@ import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 
 @Service
 public class JWTService {
 
-
     private JwtEncoder jwtEncoder;
-    
+
     public JWTService(JwtEncoder jwtEncoder) {
         this.jwtEncoder = jwtEncoder;
     }
-    
+
     public String generateToken(Authentication authentication) {
-                Instant now = Instant.now();
-            JwtClaimsSet claims = JwtClaimsSet.builder()
-                      .issuer("self")
-                      .issuedAt(now)
-                      .expiresAt(now.plus(1, ChronoUnit.DAYS))
-                      .subject(authentication.getName())
-                      .build();
-        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
-        return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                .subject(authentication.getName())
+                .build();
+        
+        JwtEncoderParameters encoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
+        Jwt jwt = this.jwtEncoder.encode(encoderParameters);
+        
+        // Extraction de la représentation textuelle du JWT
+        return jwt.getTokenValue();
     }
 }
