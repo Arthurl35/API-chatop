03faf83d4d524e3b6db89047ee770d97dd990b6c
arthur levesque <arthurlevesque35@gmail.com>
2023-12-03 17:10:06 +0100

    Création du endPoint 'updateRental' qui n'est pas 100% opérationnel, erreur dans le content-type.

--- src/main/java/com/openclassrooms/apichatop/controllers/RentalController.java
+++ src/main/java/com/openclassrooms/apichatop/controllers/RentalController.java
@@ -3,11 +3,15 @@ package com.openclassrooms.apichatop.controllers;
 import com.openclassrooms.apichatop.model.Rental;
 import com.openclassrooms.apichatop.services.RentalService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +39,17 @@ public class RentalController {
         Optional<Rental> rental = rentalService.getRentalById(id);
         return rental.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateRental(@PathVariable Long id, @RequestBody Rental updatedRental) {
+        Optional<Rental> updated = rentalService.updateRental(id, updatedRental);
+        if (updated.isPresent()) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Rental updated!");
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- src/main/java/com/openclassrooms/apichatop/services/RentalService.java
+++ src/main/java/com/openclassrooms/apichatop/services/RentalService.java
@@ -3,6 +3,7 @@ package com.openclassrooms.apichatop.services;
 import com.openclassrooms.apichatop.model.Rental;
 import com.openclassrooms.apichatop.repository.RentalRepository;
 
+import java.sql.Timestamp;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -24,4 +25,24 @@ public class RentalService {
     public Optional<Rental> getRentalById(Long id) {
         return rentalRepository.findById(id);
     }
+
+    public Optional<Rental> updateRental(Long id, Rental updatedRental) {
+        return rentalRepository.findById(id).map(rental -> {
+            if (updatedRental.getName() != null) {
+                rental.setName(updatedRental.getName());
+            }
+            if (updatedRental.getSurface() != null) {
+                rental.setSurface(updatedRental.getSurface());
+            }
+            if (updatedRental.getPrice() != null) {
+                rental.setPrice(updatedRental.getPrice());
+            }
+            if (updatedRental.getDescription() != null) {
+                rental.setDescription(updatedRental.getDescription());
+            }
+            rental.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+            return rentalRepository.save(rental);
+        });
+    }
+    
 }
