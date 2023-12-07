2075d119dd14cc09709f0affece5730c2bb777b1
arthur levesque <arthurlevesque35@gmail.com>
2023-12-07 16:19:02 +0100

    Création du endPoint 'Register' et amélioration du code, bien utiliser les services dans les controllers que les répository.

--- src/main/java/com/openclassrooms/apichatop/controllers/AuthController.java
+++ src/main/java/com/openclassrooms/apichatop/controllers/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.openclassrooms.apichatop.model.User;
 import com.openclassrooms.apichatop.repository.UserRepository;
 import com.openclassrooms.apichatop.services.JWTService;
+import com.openclassrooms.apichatop.services.UserService;
 
 
 @RestController
@@ -21,20 +22,20 @@ import com.openclassrooms.apichatop.services.JWTService;
 public class AuthController {
 
 	private JWTService jwtService;
-	private UserRepository userRepository;
+	private UserService userService;
 	
-	public AuthController(JWTService jwtService, UserRepository userRepository) {
+	public AuthController(JWTService jwtService, UserService userService) {
 		this.jwtService = jwtService;
-		this.userRepository = userRepository;
+		this.userService = userService;
 	}
 	
-@PostMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<String> getToken(@RequestBody Map<String, String> loginDetails) {
         String login = loginDetails.get("login");
         String password = loginDetails.get("password");
 
         // Vérifier si l'utilisateur existe dans la base de données avec le login donné
-        User user = userRepository.findByEmail(login);
+        User user = userService.getUserByEmail(login);
 
         if (user != null && password.equals(user.getPassword())) {
             // Utilisateur trouvé et mot de passe correspondant
@@ -46,4 +47,31 @@ public class AuthController {
         }
     }
 	
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Map<String, String> registrationDetails) {  
+        String email = registrationDetails.get("email");
+        String name = registrationDetails.get("name");
+        String password = registrationDetails.get("password");
+        if (email == null || name == null || password == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email, name, and password are required");
+        }
+
+        // Vérifier si l'utilisateur existe déjà avec cet email en utilisant le service
+        boolean userExists = userService.isUserExistByEmail(email);
+        if (userExists) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User with this email already exists");
+        }
+
+        try {
+            // Utiliser le service pour créer un nouvel utilisateur
+            User newUser = userService.createUser(email, name, password);
+
+            // Générer un token pour le nouvel utilisateur enregistré
+            String token = jwtService.generateToken(newUser);
+
+            return ResponseEntity.ok(token);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/openclassrooms/apichatop/services/UserService.java
+++ src/main/java/com/openclassrooms/apichatop/services/UserService.java
@@ -16,5 +16,29 @@ public class UserService {
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
+
+    public boolean isUserExistByEmail(String email) {
+        User existingUser = getUserByEmail(email);
+        return existingUser != null;
+    }
+
+    public User createUser(String email, String name, String password) {
+        // Vérifier si l'utilisateur existe déjà avec cet email
+        User existingUser = userRepository.findByEmail(email);
+        if (existingUser != null) {
+            throw new IllegalArgumentException("User with this email already exists");
+        }
+
+        // Créer un nouvel utilisateur
+        User newUser = new User();
+        newUser.setEmail(email);
+        newUser.setName(name);
+        newUser.setPassword(password); // Assurez-vous de stocker les mots de passe de manière sécurisée dans la réalité
+
+        // Enregistrer le nouvel utilisateur dans la base de données
+        userRepository.save(newUser);
+
+        return newUser;
+    }
    
 }
