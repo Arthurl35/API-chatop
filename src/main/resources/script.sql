eb0b69fd7b6926d048642aa447ff5934a254c148
Arthur LEVESQUE <98586610+Arthurl35@users.noreply.github.com>
2024-01-08 17:46:56 +0100

    Update script.sql avec création et utilisation de la bdd directement

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,38 +1,39 @@
+CREATE DATABASE rentals;
+USE rentals;
+
 CREATE TABLE `USERS` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `email` varchar(255),
-  `name` varchar(255),
-  `password` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `email` VARCHAR(255),
+  `name` VARCHAR(255),
+  `password` VARCHAR(255),
+  `created_at` TIMESTAMP,
+  `updated_at` TIMESTAMP
 );
 
 CREATE TABLE `RENTALS` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `surface` numeric,
-  `price` numeric,
-  `picture` varchar(255),
-  `description` varchar(2000),
-  `owner_id` integer NOT NULL,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255),
+  `surface` NUMERIC,
+  `price` NUMERIC,
+  `picture` VARCHAR(255),
+  `description` VARCHAR(2000),
+  `owner_id` INT NOT NULL,
+  `created_at` TIMESTAMP,
+  `updated_at` TIMESTAMP,
+  FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`)
 );
 
 CREATE TABLE `MESSAGES` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `rental_id` integer,
-  `user_id` integer,
-  `message` varchar(2000),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `rental_id` INT,
+  `user_id` INT,
+  `message` VARCHAR(2000),
+  `created_at` TIMESTAMP,
+  `updated_at` TIMESTAMP,
+  FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`)
 );
 
 CREATE UNIQUE INDEX `USERS_index` ON `USERS` (`email`);
 
-ALTER TABLE `USERS` ADD FOREIGN KEY (`id`) REFERENCES `RENTALS` (`owner_id`);
-
-ALTER TABLE `USERS` ADD FOREIGN KEY (`id`) REFERENCES `MESSAGES` (`user_id`);
-
-ALTER TABLE `RENTALS` ADD FOREIGN KEY (`id`) REFERENCES `MESSAGES` (`rental_id`);
 
